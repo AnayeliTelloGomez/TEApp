462859ab44d9a7ec3548449784247edede84ed5d
Anayeli <anayelitg01@gmail.com>
2024-04-25 19:22:22 -0600

    rutasAgregadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { AltaPacienteComponent } from './components/alta-paciente/alta-paciente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    {path: 'registro', component: AltaPacienteComponent},
+
+];
+
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuxCheckComponent } from '../display-mode/aux-check.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AuxCheckComponent],
+  imports: [AuxCheckComponent, RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -12,5 +13,9 @@ export class HeaderComponent {
   theme: string = 'light'; // Por defecto, se establece el tema en auto
   setTheme(theme: string) {
     document.documentElement.setAttribute('data-bs-theme', theme);
+  }
+
+  onInicio(){
+
   }
 }
