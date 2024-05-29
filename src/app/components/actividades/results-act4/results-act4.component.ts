d991b5daac91dc88a0402347d3212964e132e3b0
KevinSVK <ksuarezvaz@outlook.com>
2024-05-29 14:46:37 -0600

    Guardado de estadisticas en actividades 3,4,5 así como cambiar su valor a realizadas.

--- src/app/components/actividades/results-act4/results-act4.component.ts
+++ src/app/components/actividades/results-act4/results-act4.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { conexionAzFuncService } from '../../../services/conexionAzFunc.service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-results-act4',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule,RouterLink,RouterOutlet, RouterLinkActive],
   templateUrl: './results-act4.component.html',
   styleUrl: './results-act4.component.css'
 })
-export class ResultsAct4Component {
+export class ResultsAct4Component implements OnInit {
   correctCount: number = 0;
   incorrectCount: number = 0;
   elapsedTime: number = 0;
+  idact: number=0;
+  altaSuccess: boolean =false;
+  altaError: boolean =true;
+  submitted: boolean= false;
+  altaMessage: string='';
+
+  multi: any[] = [];
+  multi2: any[] = [];
   
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private route: ActivatedRoute, private conexionAzFunc:conexionAzFuncService) {
     const navigation = this.router.getCurrentNavigation();
     if (navigation && navigation.extras.state) {
       const state = navigation.extras.state as { incorrectCount: number, correctCount: number, elapsedTime: number };
@@ -23,5 +34,124 @@ export class ResultsAct4Component {
       this.elapsedTime = state.elapsedTime;
     }
   }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.idact = Number(params.get('idact')) || 1;
+      console.log(this.idact);
+    });
+
+
+    // Inicializa `multi` con los valores correctos
+    this.multi = [
+      {
+        "name": "Actividad 3",
+        "series": [
+         {
+           "name": "Correctas",
+            "value": this.correctCount
+          },
+          {
+           "name": "Incorrectas",
+           "value": this.incorrectCount
+          }
+        ]
+      }
+      ];
+
+
+
+  // Inicializa `multi2` con los valores correctos
+  this.multi2 = [
+    {
+      "name": "Actividad 3",
+      "series": [
+        {
+          "name": "0",
+           "value": "0"
+        },
+       {
+         "name": "1",
+          "value": this.elapsedTime
+        }
+      ]
+    }
+    ];
+
+  }
+
+
+
+  view: [number, number] = [600, 300];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Actividad';
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Numero de Reactivos';
+  legendTitle: string = 'Respuestas';
+
+
+  onSelect(data : any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data : any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data : any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+
+
+
+
+  // options
+  showXAxis2: boolean = true;
+  showYAxis2: boolean = true;
+  gradient2: boolean = true;
+  showXAxisLabel2: boolean = true;
+  xAxisLabel2: string = 'Tiempo de Actividad';
+  showYAxisLabel2: boolean = true;
+  yAxisLabel2: string = ' Segundos';
+
+
+
+
+
+  guardarEst(){
+    console.log(this.correctCount+' '+this.incorrectCount+' '+this.elapsedTime);
+    const estadistica = {
+      idact:this.idact+'',
+      correctas: this.correctCount+'',
+      incorrectas: this.incorrectCount+'',
+      tiempo: this.elapsedTime+''
+    };
+    console.log(estadistica)
+
+    this.conexionAzFunc.guardarEstadistica(estadistica)
+      .subscribe({
+        next: (response) => {
+          this.altaSuccess = true;
+          this.altaError=true;
+          this.altaMessage = response.message;
+          this.submitted=false;
+        },
+        error: (error) => {
+          this.altaError=false;
+          this.altaSuccess=false;
+          console.error('Error al guardas los datos: ', error);
+          this.altaMessage = 'Ocurrión un error mientras se guardaban los cambios, intente de nuevo.';
+        }
+      });
+
+  }
+
 
 }
--- src/app/components/actividades/results-act5/results-act5.component.ts
+++ src/app/components/actividades/results-act5/results-act5.component.ts
@@ -1,30 +1,157 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { conexionAzFuncService } from '../../../services/conexionAzFunc.service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 
 @Component({
   selector: 'app-results-act5',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule,RouterLink,RouterOutlet, RouterLinkActive],
   templateUrl: './results-act5.component.html',
   styleUrl: './results-act5.component.css'
 })
 export class ResultsAct5Component implements OnInit {
 
 
-  correctas: number = 0;
-  incorrectas: number = 0;
-  tiempo: number = 0;
+  correctCount: number = 0;
+  incorrectCount: number = 0;
+  elapsedTime: number = 0;
+  idact: number=0;
+  altaSuccess: boolean =false;
+  altaError: boolean =true;
+  submitted: boolean= false;
+  altaMessage: string='';
 
-  constructor(private router: Router) {
+  multi: any[] = [];
+  multi2: any[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute, private conexionAzFunc:conexionAzFuncService) {
     const navigation = this.router.getCurrentNavigation();
     if (navigation && navigation.extras.state) {
       const state = navigation.extras.state as { correctas: number, incorrectas: number, tiempo: number };
-      this.correctas = state.correctas;
-      this.incorrectas = state.incorrectas;
-      this.tiempo = state.tiempo;
+      this.correctCount = state.correctas;
+      this.incorrectCount = state.incorrectas;
+      this.elapsedTime = state.tiempo;
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe(params => {
+      this.idact = Number(params.get('idact')) || 1;
+      console.log(this.idact);
+    });
+
+
+    // Inicializa `multi` con los valores correctos
+    this.multi = [
+      {
+        "name": "Actividad 3",
+        "series": [
+         {
+           "name": "Correctas",
+            "value": this.correctCount
+          },
+          {
+           "name": "Incorrectas",
+           "value": this.incorrectCount
+          }
+        ]
+      }
+      ];
+
+
+
+  // Inicializa `multi2` con los valores correctos
+  this.multi2 = [
+    {
+      "name": "Actividad 3",
+      "series": [
+        {
+          "name": "0",
+           "value": "0"
+        },
+       {
+         "name": "1",
+          "value": this.elapsedTime
+        }
+      ]
+    }
+    ];
+
+  }
+
+  view: [number, number] = [600, 300];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Actividad';
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Numero de Reactivos';
+  legendTitle: string = 'Respuestas';
+
+
+  onSelect(data : any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data : any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data : any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+
+
+
+
+  // options
+  showXAxis2: boolean = true;
+  showYAxis2: boolean = true;
+  gradient2: boolean = true;
+  showXAxisLabel2: boolean = true;
+  xAxisLabel2: string = 'Tiempo de Actividad';
+  showYAxisLabel2: boolean = true;
+  yAxisLabel2: string = ' Segundos';
+
+
+
+
+
+  guardarEst(){
+    console.log(this.correctCount+' '+this.incorrectCount+' '+this.elapsedTime);
+    const estadistica = {
+      idact:this.idact+'',
+      correctas: this.correctCount+'',
+      incorrectas: this.incorrectCount+'',
+      tiempo: this.elapsedTime+''
+    };
+    console.log(estadistica)
+
+    this.conexionAzFunc.guardarEstadistica(estadistica)
+      .subscribe({
+        next: (response) => {
+          this.altaSuccess = true;
+          this.altaError=true;
+          this.altaMessage = response.message;
+          this.submitted=false;
+        },
+        error: (error) => {
+          this.altaError=false;
+          this.altaSuccess=false;
+          console.error('Error al guardas los datos: ', error);
+          this.altaMessage = 'Ocurrión un error mientras se guardaban los cambios, intente de nuevo.';
+        }
+      });
+
+  }
 
 }
