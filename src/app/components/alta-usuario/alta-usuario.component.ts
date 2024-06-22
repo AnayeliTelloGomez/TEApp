aa33899a4ee31a6a519cce9ea14f3a0e7ad0d7a5
Anayeli <anayelitg01@gmail.com>
2024-06-22 14:44:57 -0600

    validación de contraseña completa

--- src/app/components/alta-usuario/alta-usuario.component.ts
+++ src/app/components/alta-usuario/alta-usuario.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, NgModule} from '@angular/core';
 //importa el servicio para la solicitud http
 import { conexionAzFuncService } from '../../services/conexionAzFunc.service';
+//validar formulario
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 //importa la interfaz para manejar el objt paciente
 import { Usuario } from '../../models/usuario.interface';
-import { Error } from '../../models/error.interface';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
@@ -31,6 +32,8 @@ export class AltaUsuarioComponent implements OnInit{
   submitted: boolean= false;
   altaMessage: string='';
 
+  pass= new FormControl('',Validators.required);
+
   //instancia servicio
   constructor (private conexionAzFuncService: conexionAzFuncService){ }
 
@@ -40,7 +43,17 @@ export class AltaUsuarioComponent implements OnInit{
   setTheme(theme: string) {
     document.documentElement.setAttribute('data-bs-theme', theme);
   }
-  
+
+  passwordInvalid: boolean = false;
+
+  validatePassword() {
+    const password = this.contrasena;
+    const hasUpper = /[A-Z]/.test(password);
+    const hasLower = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecial = /[^\w]/.test(password);
+    return this.passwordInvalid = !hasUpper || !hasLower || !hasNumber || !hasSpecial;
+  }
 
   register(){
     this.submitted=true;
@@ -58,7 +71,7 @@ export class AltaUsuarioComponent implements OnInit{
         next: (response) => {
           this.altaSuccess = true;
           this.altaError=true;
-          this.altaMessage = response as string;
+          this.altaMessage = response.message;
           this.correo = '';
           this.contrasena = '';
           this.nombres = '';
@@ -68,10 +81,12 @@ export class AltaUsuarioComponent implements OnInit{
           this.submitted=false;
         },
         error: (error) => {
-          this.altaError=false;
+          
           this.altaSuccess=false;
           console.error('Error al registrar paciente: ', error);
           this.altaMessage = error.error.mensaje;//'Ocurrión un error mientras se registraba el usuario, intente de nuevo.';
+          if(this.altaMessage== 'Ya hay un usuario con ese correo electrónico.')
+            this.altaError=false;
         }
       });
   }
